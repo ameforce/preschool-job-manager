169a263ba97333167f18d2c1af2762c7bba78448
Jongin Kim <51025425+ameforce@users.noreply.github.com>
2023-11-11 21:20:34 +0900

    feat: Extracting subregions

--- PreschoolJobManager.py
+++ PreschoolJobManager.py
@@ -8,37 +8,41 @@ import time
 
 class PreschoolJobManager:
     def __init__(self, default_url: str = 'https://www.gogane.kr/c/main.cgi?board=gi2'):
-        self.driver = webdriver.Chrome()
-        self.default_url = default_url
-        self.region_list = []
+        self.__driver = webdriver.Chrome()
+        self.__default_url = default_url
+        self.__region_list = None
+        self.__subregion_list = None
         return
 
-    def __move_site(self, url: str) -> None:
-        self.driver.get(url)
+    def get_default_url(self) -> str:
+        return self.__default_url
+
+    def move_site(self, url: str) -> None:
+        self.__driver.get(url)
         return
 
     def __switch_frame(self, frame_name: str = None) -> bool:
         if frame_name is not None:
             try:
-                self.driver.switch_to.frame(frame_name)
+                self.__driver.switch_to.frame(frame_name)
             except NoSuchFrameException:
                 try:
-                    self.driver.find_element(By.XPATH, f'//*[@id="{frame_name}"]')
+                    self.__driver.find_element(By.XPATH, f'//*[@id="{frame_name}"]')
                 except NoSuchElementException:
                     return False
         return True
 
     def __get_element(self, element_xpath: str, frame_name: str = None, implicitly_wait: float = 2) -> WebElement or None:
-        self.driver.implicitly_wait(implicitly_wait)
+        self.__driver.implicitly_wait(implicitly_wait)
         element = None
         if self.__switch_frame(frame_name):
             try:
-                element = self.driver.find_element(By.XPATH, element_xpath)
+                element = self.__driver.find_element(By.XPATH, element_xpath)
             except NoSuchElementException:
                 pass
         return element
 
-    def __get_region_list(self) -> list[str]:
+    def extract_region_list(self) -> list[str]:
         region_list = []
         i = 1
         while True:
@@ -49,23 +53,51 @@ class PreschoolJobManager:
             i += 1
         return region_list
 
-    def __set_region_list(self, region_list: list[str]) -> None:
-        self.region_list = region_list
+    def get_region_list(self) -> list[str]:
+        return self.__region_list
+
+    def set_region_list(self, region_list: list[str]) -> None:
+        self.__region_list = region_list
         return
 
-    @staticmethod
-    def __print_selection(print_list: list[str]) -> None:
+    def print_and_region_chooser(self) -> int:
+        if self.__region_list is None:
+            raise Exception('지역이 설정되지 않았습니다.\n지역을 먼저 설정해야 사용 가능합니다.')
         print('원하는 지역을 선택해주세요')
-        for i in range(len(print_list)):
-            print(f'{i}. {print_list[i]}')
-        input('-->')
+        for i in range(len(self.__region_list)):
+            print(f'{i + 1}. {self.__region_list[i]}')
+        choose_num = int(input('-->'))
+        return choose_num
+
+    def select_region(self, region_num: int) -> None:
+        self.__get_element(f'/html/body/div/table/tbody/tr[1]/td[1]/a[{region_num}]').click()
         return
 
-    def logic(self) -> None:
-        self.__move_site(self.default_url)
-        self.__set_region_list(self.__get_region_list())
-        self.__print_selection(self.__region)
-        print(f'region: {self.region_list}')
+    def extract_subregion(self) -> list[str]:
+        subregion_list = []
+        i = 0
+        while True:
+            # element = self.__get_element(f'/html/body/div/table/tbody/tr[1]/td[1]/table[1]/tbody/tr[{3+i}]/td[1]')
+            element = self.__get_element(f'/html/body/div/table/tbody/tr[1]/td[1]/table[2]/tbody/tr[{3+i}]/td[1]/b',
+                                         implicitly_wait=0)
+            if element is not None:
+                i += 1
+                continue
 
-        time.sleep(5090)
+            element = self.__get_element(f'/html/body/div/table/tbody/tr[1]/td[1]/table[2]/tbody/tr[{3+i}]/td[1]',
+                                         implicitly_wait=0)
+            i += 1
+            if element is None:
+                break
+            if element.text == '' or element.text in subregion_list:
+                continue
+            subregion_list.append(element.text)
+            print(f'INFO: {i-1}: {element.text}')
+        return subregion_list
+
+    def get_subregion_list(self) -> list[str]:
+        return self.__subregion_list
+
+    def set_subregion_list(self, subregion_list: list[str]) -> None:
+        self.__subregion_list = subregion_list
         return
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from PreSchoolJobManager import PreschoolJobManager
+from PreschoolJobManager import PreschoolJobManager
+import time
 
 
 def main() -> None:
-    Getter().logic()
+    try:
+        pm = PreschoolJobManager()
+        pm.move_site(pm.get_default_url())
+        pm.set_region_list(pm.extract_region_list())
+        pm.select_region(pm.print_and_region_chooser())
+        pm.set_subregion_list(pm.extract_subregion())
+        print(pm.get_subregion_list())
+        time.sleep(5000)
+    except Exception as e:
+        print(e)
     return
 
 
